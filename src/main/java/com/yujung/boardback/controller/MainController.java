fc280ca82ed9ccd92618a833fa104dc4adb2a32e
kimyujung <yjangel3702@gmail.com>
2023-09-21 13:02:22 +0900

    오잉

---
+++ src/main/java/com/yujung/boardback/controller/MainController.java
@@ -0,0 +1,10 @@
+package com.yujung.boardback.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class MainController {
+  
+}
---
+++ src/main/java/com/yujung/boardback/service/implement/AuthServiceImplement.java
@@ -0,0 +1,10 @@
+package com.yujung.boardback.service.implement;
+
+import org.springframework.stereotype.Service;
+
+import com.yujung.boardback.service.AuthService;
+
+@Service
+public class AuthServiceImplement implements AuthService {
+  
+}
