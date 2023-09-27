0b712c19f14dc82871d98600c9cdff665dba1954
kimyujung <yjangel3702@gmail.com>
2023-09-27 10:59:08 +0900

    어렵다

--- src/main/java/com/yujung/boardback/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/yujung/boardback/filter/JwtAuthenticationFilter.java
@@ -34,6 +34,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     try {
       
         String token = parseBearerToken(request);
+        System.out.println(token);
         if (token == null) {
             filterChain.doFilter(request, response);
             return;
