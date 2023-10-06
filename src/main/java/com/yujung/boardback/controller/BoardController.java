b641aea3d947517a3c3a29c2b8311f457e867479
kimyujung <yjangel3702@gmail.com>
2023-10-06 13:43:16 +0900

    아악

--- src/main/java/com/yujung/boardback/controller/BoardController.java
+++ src/main/java/com/yujung/boardback/controller/BoardController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.yujung.boardback.dto.request.board.PatchBoardRequestDto;
 import com.yujung.boardback.dto.request.board.PostBoardRequestDto;
 import com.yujung.boardback.dto.request.board.PostCommentRequestDto;
 import com.yujung.boardback.dto.response.board.GetBoardResponseDto;
 import com.yujung.boardback.dto.response.board.GetCommentListResponseDto;
 import com.yujung.boardback.dto.response.board.GetFavoriteListResponseDto;
 import com.yujung.boardback.dto.response.board.GetLatestBoardListResponseDto;
+import com.yujung.boardback.dto.response.board.PatchBoardResponseDto;
 import com.yujung.boardback.dto.response.board.PostBoardResponseDto;
 import com.yujung.boardback.dto.response.board.PostCommentResponseDto;
 import com.yujung.boardback.dto.response.board.PutFavoriteResponseDto;
@@ -90,4 +93,13 @@ public class BoardController {
     return response;
   }
 
+  @PatchMapping("/{boardNumber}")
+  public ResponseEntity<? super PatchBoardResponseDto> patchBoard(
+    @RequestBody @Valid PatchBoardRequestDto requestBody,
+    @PathVariable("boardNumber") Integer boardNumber,
+    @AuthenticationPrincipal String email
+  ) {
+    ResponseEntity<? super PatchBoardResponseDto> response = boardService.patchBoard(requestBody, boardNumber, email);
+    return response;
+  }
 }
